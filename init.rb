3a746dea47f47be915538789ec98ce8461078f8d
Jules Delecour <jules.delecour@davidson.fr>
2022-03-22 18:33:47 +0100

    updating en

--- init.rb
+++ init.rb
@@ -3,8 +3,8 @@ Redmine::Plugin.register :alertmanager do
   author 'Jules Delecour'
   description 'This is a plugin for Redmine'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/davidson-consulting/redmine-alertmanager-plugin.git'
+  author_url 'https://github.com/davidson-consulting'
   settings default: {'empty' => true}, partial: 'settings/alertmanagerapi_settings'
   project_module :alertmanagerapi do
     permission :create_alert, {:alertmanagerapi => [:catch]}, :require => :member
