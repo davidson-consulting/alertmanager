235e6dd476dc98a7e281b9049010e2b9b4e0d8d4
Jules Delecour <72793427+jules-delecour-dav@users.noreply.github.com>
2022-04-05 20:25:16 +0200

    Update alertmanagerapi_controller.rb

--- app/controllers/alertmanagerapi_controller.rb
+++ app/controllers/alertmanagerapi_controller.rb
@@ -23,6 +23,11 @@ class AlertmanagerapiController < ApplicationController
                 issue.save
             end
 
+            if k["status"] == "firing" and Issue.where(:subject => id).any? == true and Issue.where(:status => IssueStatus.find_by_name("resolved")).any? == true
+                issue = Issue.where(:subject => id, :status => IssueStatus.find_by_name("resolved"))
+                issue.update(:status => IssueStatus.find_by_name("firing"))
+            end
+            
             if k["status"] == "resolved" and Issue.where(:subject => id).any? == true and Issue.where(:status => IssueStatus.find_by_name("firing")).any? == true
                 issue = Issue.where(:subject => id, :status => IssueStatus.find_by_name("firing"))
                 issue.update(:status => IssueStatus.find_by_name("resolved"))
